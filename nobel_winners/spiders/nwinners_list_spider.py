1b24dad9623e18f4f31264ff2347bb20b4de8ed6
DecaHub <dan@decahub.com>
2016-11-20 20:25:44 -0500

    Add method to process a winner's <li> selector and extract: link, name, year, category, country, norn_in

--- nobel_winners/spiders/nwinners_list_spider.py
+++ nobel_winners/spiders/nwinners_list_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 import re
 
+BASE_URL = 'http://en.wikipedia.org'
+
 
 # Define the data to be scraped
 class NWinnterItem(scrapy.Item):
@@ -49,10 +51,61 @@ class NWinnerSpider(scrapy.Spider):
             country = h2.xpath('span[@class="mw-headline"]/text()').extract()
             if country:
                 winners = h2.xpath('following-sibling::ol[1]')[0]
-                for w in winners.xpath('li'):
-                    text = w.xpath('descendant-or-self::text()').extract()
-                    yield NWinnterItem(
-                        country=country[0],
-                        name=text[0],
-                        link_text=' '.join(text)
-                    )
+                for winner in winners.xpath('li'):
+                    winner_data = process_winner_li(winner, country[0])
+                    yield  NWinnterItem(winner_data)
+                    # yield NWinnterItem(
+                    #     country=country[0],
+                    #     name=text[0],
+                    #     link_text=' '.join(text)
+                    # )
+
+
+def process_winner_li(winner, country=None):
+    """
+    Process a winner's <li> tag, adding country of birth or nationality,
+    as applicable
+    :param winner: <li> selector of winner
+    :param country: country under which winner is found
+    :return: processed winner data object
+    """
+
+    winner_data = {}
+
+    link = winner.xpath('a/@href').extract()[0]
+
+    if link:
+        winner_data['link'] = BASE_URL + link
+
+    text = winner.xpath('descendant-or-self::text()').extract()
+
+    if text:
+        text = ' '.join(text)
+        # We expect the first element of the list in text to be the name
+        name = text.split(',')[0].strip()
+        if name:
+            winner_data['name'] = name
+
+        year = re.findall('\d{4}', text)
+        if year:
+            winner_data['year'] = int(year[0])
+        else:
+            winner_data['year'] = 0
+            print('Oops, no year in ', text)
+
+        category = re.findall('Physics|Chemistry|Physiology or Medicine|Literature|Peace|Economics', text)
+        if category:
+            winner_data['category'] = category[0]
+
+        if country:
+            if text.find('*') != -1:
+                winner_data['country'] = ''
+                winner_data['born_in'] = country
+            else:
+                winner_data['country'] = country
+                winner_data['born_in'] = ''
+
+        winner_data['text'] = text
+
+    print(winner_data)
+    return winner_data
